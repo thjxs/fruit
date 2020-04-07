a6f39b632c51bc3fe30ec033cb3aa2a8c3d78f5b
thjxs <tanhongjianx@163.com>
2020-04-07 14:12:03 +0800

    Add missing style rule

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,15 @@
 const purgecss = require('@fullhuman/postcss-purgecss')({
-  content: [
-    './app/ui/*.js'
-  ],
-  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-})
+  content: ['./app/ui/*.js', './app/*.js'],
+  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+});
 
 const options = {
-  plugins: [require('tailwindcss')]
+  plugins: [require('tailwindcss')],
 };
 
 if (process.env.NODE_ENV === 'production') {
-  options.plugins.push(purgecss)
-  options.plugins.push(require('cssnano'))
+  options.plugins.push(purgecss);
+  options.plugins.push(require('cssnano'));
 }
 
-module.exports = options
+module.exports = options;
