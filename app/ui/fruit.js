513542a6500c8ccf61fafc995605c5693a060661
thjxs <tanhongjianx@163.com>
2020-08-13 18:13:46 +0800

    style

--- app/ui/fruit.js
+++ app/ui/fruit.js
@@ -21,16 +21,28 @@ export default function fruit(state, loading) {
     `;
   }
   return html`
-    <div class="mx-auto">
+    <div class="mx-auto fruit-container">
       <img
         id="fruit-img"
         class="fruit-img opacity-0 mx-auto"
         src="/images/${state.hash}"
       />
-      <h2 class="fruit-text">Artist: ${state.artist}</h2>
-      <p class="fruit-text">year: ${state.year}</p>
-      <p class="fruit-text">Scientific name: ${state.scientific_name}</p>
-      <p class="fruit-text">Common name: ${state.common_name}</p>
+      <h2 class="fruit-text text-xl">
+        <span class="inline-block w-32 text-right mr-4">Artist: </span
+        >${state.artist}
+      </h2>
+      <p class="fruit-text">
+        <span class="inline-block w-32 text-right mr-4">year: </span
+        >${state.year}
+      </p>
+      <p class="fruit-text">
+        <span class="inline-block w-32 text-right mr-4">Scientific name: </span
+        >${state.scientific_name}
+      </p>
+      <p class="fruit-text">
+        <span class="inline-block w-32 text-right mr-4">Common name: </span
+        >${state.common_name}
+      </p>
     </div>
   `;
 }
